9293aef119d5449181fee15741c69c54f158e772
wendellcalixtoo <wendellcalixtoo>
2019-08-07 07:46:37 -0300

    Node connection

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const mysql = require('mysql');
+const server = express();
 
-const server =express();
+const connection = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: '',
+    database: 'sampleDB'
+});
 
-server.get('/', ( req, res) => {
-    return res.json({ message: 'Hello World'});
+connection.connect(function(error){
+    if (!!error){
+        console.log('Error!!!');
+    } else {
+        console.log('Connected \n');
+        console.log('run localhost:3000/ \n');
+    }
+});
+
+server.get('/', function(req, resp){
+    connection.query("SELECT * FROM mySampleTable", function(error, rows, fields){
+        if(!!error){
+            console.log('Error in the query');
+        } else {
+            console.log('Sucess!\n');
+            console.log(rows[0]);
+            resp.json(rows[0]);
+        }
+    })
 })
 
 server.listen(3000);
